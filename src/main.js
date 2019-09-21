7d0d0c514b0c96b946297c7e0e6be27446690a55
tatsuya-n <i8658i8658@gmail.com>
2019-09-21 12:37:59 +0900

    Add: axios

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './root/router'
+import vuetify from './plugins/vuetify';
+import VueLogger from 'vuejs-logger';
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
+  vuetify,
+  VueLogger,
   render: h => h(App),
 }).$mount('#app')
